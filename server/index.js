38a67e2e2cc2c1a95a513ff18fa61e0094b1d6ef
shriramsadawarti@gmail.com <shriramsadawarti@gmail.com>
2026-03-25 06:30:15 +0000

    created landing.ejs

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Hello World 🚀");
 });
 
+app.get("/Hot-Stay", (req, res) => {
+    res.send("Welcome to Hot Stay! 🚀");
+});
+
 // server start
 app.listen(3000, () => {
     console.log("Server running on port 3000");
